7670155457fbc815944f76845e22f2befff952e5
spin <spin@fluorescence-client.org>
2012-07-01 17:05:23 +0200

    small changes to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 rm packages/win32.zip
 cp fluorescence_win32.exe packages/win32
+mkdir packages/win32/shards
 
-zip -9 -r packages/win32.zip shards/example/ fonts/ gumps/ shader/ themes/default/ data/ LICENSE effects/
+zip -9 -r packages/win32.zip fonts/ gumps/ shader/ themes/default/ data/ LICENSE effects/ defaultConfig.xml credits.txt
 cd packages/win32
 zip -9 ../win32.zip *
 
